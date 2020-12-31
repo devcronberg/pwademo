0c719fa770357d0ad736e64ee638da136f8fbe72
Michell Cronberg <michell@cronberg.dk>
2020-12-31 13:01:14 +0100

    images

--- scripts/forside.js
+++ scripts/forside.js
@@ -2,7 +2,10 @@ import * as shared from "./shared.js";
 
 let html = `<div>Dette er et eksempel på en simpel PWA applikation. Den er skabt i forbindelse med en
   artikel skrevet af <a href="https://blog.cronberg.dk/om">Michell Cronberg</a> og bragt i
-  <a href="https://www.aod.dk">Alt om DATA</a> i 2021.</div>`;
+  <a href="https://www.aod.dk">Alt om DATA</a> i 2021.</div>
+  <ul><li>Billeder/icon er fra <a href="https://favicon.io/emoji-favicons/test-tube">favicon.io</a></li>
+  </ul>
+  `;
 
 export function render() {
   shared.skrivHTML("#app", html);
